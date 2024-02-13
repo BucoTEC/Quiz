50fe8a67ba2d8a3c0209889f7941aa3d7c800bde
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2024-02-13 15:38:42 +0100

    add creat quiz and get quiz by id functionality

--- src/Quiz.Api/Controllers/QuizzesController.cs
+++ src/Quiz.Api/Controllers/QuizzesController.cs
@@ -3,13 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Quiz.Bll.Dtos;
+using Quiz.Bll.Services.QuizService;
 
 namespace Quiz.Api.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class QuizzesController : ControllerBase
+    public class QuizzesController(IQuizService quizService) : ControllerBase
     {
+        private readonly IQuizService _quizService = quizService;
 
+
+        [HttpPost]
+        public async Task<ActionResult<QuizResponseDto>> CreateQuiz([FromBody] CreateQuizDto createQuizDto)
+        {
+            return await _quizService.CreateQuiz(createQuizDto);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<QuizResponseDto>> GetQuizById([FromRoute] Guid id)
+        {
+            return await _quizService.GetQuizById(id);
+        }
     }
 }
